f5daee7330f3ee3490c42386a1efeb1d5dd36df2
Patrick <patrick@godknows.de>
2022-07-01 21:09:46 +0200

    Add project files.

---
+++ BlazorIdentityServerTutorial/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorIdentityServerTutorial.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorIdentityServerTutorial.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ BlazorIdentityServerTutorial/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorIdentityServerTutorial.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
